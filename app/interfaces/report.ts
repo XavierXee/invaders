2d14b680b27fc2fc2c5cf3ee8144bc7bbf41e36d
Xavier de Frutos <xavier.defrutos@gmail.com>
2019-07-07 22:53:31 +0200

    fix git-ignore

--- app/interfaces/report.ts
+++
@@ -1,8 +0,0 @@
-import { Coord } from './coord';
-import {SpaceInvader} from "../core/space-invader";
-
-export interface Report {
-    coord: Coord;
-    target: SpaceInvader;
-    diff: number;
-}
--- src/components/RadarGrid/Radar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Radar from './Radar';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Radar />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
